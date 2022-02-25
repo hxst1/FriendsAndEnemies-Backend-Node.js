5e5ca75cbf0e3bc4a1fe456af7552d981040c44e
Edu Ruiz <eya@MacBook-Air-de-Edu.local>
2022-02-25 22:43:02 +0100

    :art: cors and helmet added

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,17 @@
 const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
 const morgan = require("morgan");
 const { generalError, notFoundError } = require("./middlewares/errors");
 const userRoute = require("./routers/userRouter");
 
 const app = express();
 
+app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
+
+app.use(helmet());
 
 app.use("/users", userRoute);
 
