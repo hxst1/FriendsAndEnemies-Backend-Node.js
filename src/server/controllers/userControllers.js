754adf2c13c44054be36e5eb52b5157b44657548
Edu Ruiz <eya@MacBook-Air-de-Edu.local>
2022-02-25 21:53:45 +0100

    :pushpin: userController login added

---
+++ src/server/controllers/userControllers.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../../database/models/User");
+
+const loginController = async (req, res, next) => {
+  const { username, password } = req.body;
+  const findUser = await User.findOne({ username });
+
+  if (!findUser) {
+    const error = new Error("User not found");
+    error.code = 401;
+    return next(error);
+  }
+
+  const rightPassword = await bcrypt.compare(password, findUser.password);
+
+  if (!rightPassword) {
+    const error = new Error("Invalid password");
+    error.code = 401;
+    return next(error);
+  }
+
+  const UserData = {
+    name: findUser.name,
+    id: findUser.id,
+  };
+
+  const token = jwt.sign(UserData, process.env.JWT_SECRET);
+  return res.json({ token });
+};
+
+module.exports = { loginController };
