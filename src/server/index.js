af42d1a04b86b7a3a296ebefa8462ba917a04043
Edu Ruiz <eya@MacBook-Air-de-Edu.local>
2022-02-25 22:25:20 +0100

    :truck: userRouter added on index

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const morgan = require("morgan");
 const { generalError, notFoundError } = require("./middlewares/errors");
+const userRoute = require("./routers/userRouter");
 
 const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
+
+app.use("/users", userRoute);
 
 app.use(generalError);
 app.use(notFoundError);
