28d090b70366422b9bba50d576541f1993da9202
Edu Ruiz <eya@MacBook-Air-de-Edu.local>
2022-02-25 21:30:44 +0100

    :zap: model User added

---
+++ src/database/models/User.js
@@ -0,0 +1,29 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  enemies: {
+    type: [Schema.Types.ObjectId],
+    ref: "User",
+  },
+  friends: {
+    type: [Schema.Types.ObjectId],
+    ref: "User",
+  },
+});
+
+const User = model("User", userSchema, "users");
+
+module.exports = User;
